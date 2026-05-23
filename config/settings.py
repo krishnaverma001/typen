4827c1e75ac12242713d6e586373555f1c3e1c25
krishnaverma001 <krishnaverma2004.kv@gmail.com>
2026-05-24 00:43:43 +0530

    test deploy-11

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS.append(MEDIA_ROOT)
 
-WHITENOISE_ROOT = MEDIA_ROOT
+# WHITENOISE_ROOT = MEDIA_ROOT
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,19 @@ urlpatterns = [
     path('u/', include('user.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    # Temp fix, enclose it in if DEBUG:
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    # Temp fix, enclose it in if DEBUG:
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT}
+        )
+    ]
+else:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
