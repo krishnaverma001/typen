507b0e54bb5523effe25a5ffa581915fda26c5ef
krishnaverma001 <krishnaverma2004.kv@gmail.com>
2026-05-24 00:55:21 +0530

    production ready

--- config/settings.py
+++ config/settings.py
@@ -21,10 +21,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATICFILES_DIRS.append(MEDIA_ROOT)
-
-# WHITENOISE_ROOT = MEDIA_ROOT
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
