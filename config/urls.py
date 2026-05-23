96660296f245a3bdea0f33ae200892985a2ca194
krishnaverma001 <krishnaverma2004.kv@gmail.com>
2026-05-24 00:45:44 +0530

    test deploy-12

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
